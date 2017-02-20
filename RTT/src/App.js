8f0674f61e35038dfe49066eb7001e016666bc21
Sean Fernandez <scfernz@gmail.com>
2017-02-19 23:35:34 -0800

    Fixed imports

--- RTT/src/App.js
+++ RTT/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component, render } from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import Square from 'components';
-import Game from 'components';
-import Board from 'components';
+import Square from './components/Square';
+import Game from './components/Game';
+import Board from './components/Board';
 
 class App extends Component {
   render() {
